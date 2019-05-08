390f74d9460274be630fc4107f10fc9a816f19e3
AngeredBovine <justinmiller.math@gmail.com>
2019-05-08 17:41:43 -0400

    Updated deployment to create directory if one does not exist.

--- Deploy.sh
+++ Deploy.sh
@@ -2,6 +2,12 @@ sed -i 's/\r$//' DeployLocations.txt
 
 while read LINE; do
 
+    if [ ! -d "$LINE" ]; then
+    
+    	mkdir ${LINE}
+
+    fi
+
     find . -name "*.js" -exec cp {} ${LINE} \;
 
 done < DeployLocations.txt
